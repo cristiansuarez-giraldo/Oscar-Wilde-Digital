5375115a683572e763f278ded88fa558961872e9
jigth98 <daniel.ochoa@cuemby.com>
2021-11-07 22:58:32 -0500

    Starting project and basic navigation

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/assets/img");
+    eleventyConfig.addPassthroughCopy("src/assets/css");
+    eleventyConfig.addPassthroughCopy("src/assets/favicon.ico");
+    return {
+        passthroughFileCopy: true,
+        dir: {
+            input: "src",
+            output: "dist",
+        },
+    };
+};
